70ee0e6a195b9eb269b99e2e80690ea859c6c1e5
dongleivip <dongleivip@gmail.com>
2015-12-08 13:22:14 +0800

    add test for find methods and add method

--- BookMark/src/com/homework/test/XmlBookMarkDaoTest.java
+++ BookMark/src/com/homework/test/XmlBookMarkDaoTest.java
@@ -1,5 +1,8 @@
 package com.homework.test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Test;
 
 import com.homework.dao.XmlBookMarkDao;
@@ -10,11 +13,26 @@ public class XmlBookMarkDaoTest {
 	@Test
 	public void testFindBookMarkByTitle(){
 		XmlBookMarkDao dao = new XmlBookMarkDao();
-		
 		BookMark bookmark = dao.findbyTitle("标题");
-		
 		System.out.println(bookmark.toString());
+	}
+
+	@Test
+	public void testFindBookMarkByTitleAndUrl(){
+		XmlBookMarkDao dao = new XmlBookMarkDao();
+		BookMark bookmark = dao.findbyTitleAndUrl("标题", "地址");
+		System.out.println(bookmark);
+	}
+
+	@Test
+	public void testAddBookMark(){
+		XmlBookMarkDao dao = new XmlBookMarkDao();
 		
-	
+		BookMark bookmark = new BookMark();
+		bookmark.setTitle("Google");
+		bookmark.setUrl("www.google.com");
+		SimpleDateFormat df =new SimpleDateFormat("yyyy-MM-dd");
+		bookmark.setDate(df.format(new Date()));
+		dao.addBookMark(bookmark);
 	}
 }
