6bb87fca8bc8681b1e6828322ab303c7a7bdc020
dongleivip <dongleivip@gmail.com>
2015-12-09 23:52:36 +0800

    add QueryBookMarkServlet  return data by json

---
+++ BookMark/src/com/homework/web/QureyBookMarkServlet.java
@@ -0,0 +1,62 @@
+package com.homework.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONNull;
+import net.sf.json.JsonConfig;
+import net.sf.json.processors.DefaultValueProcessor;
+import com.homework.domain.BookMark;
+import com.homework.factory.BasicFactory;
+import com.homework.service.BookMarkService;
+
+public class QureyBookMarkServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -6002346336931787199L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+		BookMarkService service = BasicFactory.getFactory().getInstance(BookMarkService.class);
+
+		List<BookMark> list = service.queryAllByList();
+		
+		JsonConfig config = new JsonConfig();
+		config.registerDefaultValueProcessor(
+				Date.class, new DefaultValueProcessor(){  
+			@SuppressWarnings("rawtypes")
+			public Object getDefaultValue(Class type){
+				if (type != null && Date.class.isAssignableFrom(type)) {  
+		            return "";  
+		        }  
+		        return JSONNull.getInstance();  
+	        }  
+	    });  
+		JSONArray jsonArray = JSONArray.fromObject(list,config);
+		
+		System.out.println(jsonArray.toString());
+		
+		// 设置字符编码 避免中文乱码
+		response.setCharacterEncoding("UTF-8");
+		// 设置ContentType 为"application/x-json"
+		response.setContentType("application/x-json");
+		PrintWriter out = response.getWriter();
+		// 输出JSONObject字符串到前台
+		out.println(jsonArray.toString());
+		out.flush();
+		out.close();
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
