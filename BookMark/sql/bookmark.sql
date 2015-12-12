81a6baaa2cf79bd3395e4875aed4720232095639
dongleivip <dongleivip@gmail.com>
2015-12-13 01:25:02 +0800

    add sql script

---
+++ BookMark/sql/bookmark.sql
@@ -0,0 +1,23 @@
+/*
+SQLyog Ultimate v9.30 
+MySQL - 5.5.43-0ubuntu0.12.04.1 : Database - homework
+*********************************************************************
+*/
+
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`homework` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `homework`;
+
+/*Table structure for table `tb_bookmark` */
+
+DROP TABLE IF EXISTS `tb_bookmark`;
+
+CREATE TABLE `tb_bookmark` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `url` varchar(200) NOT NULL,
+  `created` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
